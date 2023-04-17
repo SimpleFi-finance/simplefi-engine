d39f6edaef8be24951200360dbd385931189bb6f
InigoSimpleFi <inigo@simplefi.finance>
2023-04-17 11:20:04 +0200

    Adding bins

---
+++ abi_discovery/src/bin/create_queue.rs
@@ -0,0 +1,42 @@
+
+// create fn main that uses third_parties helper to create a queue
+use lapin;
+
+use abi_discovery::settings::load_settings;
+use third_parties::broker::{create_rmq_channel, declare_exchange, declare_rmq_queue, bind_queue_to_exchange};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Load rabbit url from settings with load_settings helper
+    let settings = load_settings().expect("Failed to load settings");
+    let rabbit_uri = settings.rabbit_mq_url.to_string();
+
+    let queue_name = settings.rabbit_exchange_name.to_string();
+    let exchange_name = format!("{}_exchange", queue_name);
+    let routing_key = String::from("abi_discovery");
+
+    println!("Rabbit URI: {}", rabbit_uri);
+    println!("Rabbit Queue Name: {}", queue_name);
+    println!("Rabbit Exchange Name: {}", exchange_name);
+
+    let channel = create_rmq_channel(&rabbit_uri.as_str())
+        .await
+        .expect("Failed to create channel");
+
+    // Create a queue with declare_exchange helper
+    declare_exchange(
+        &rabbit_uri.as_str(),
+        &exchange_name,
+        &lapin::ExchangeKind::Direct,
+    ).await.expect("Failed to declare exchange");
+
+    declare_rmq_queue(&queue_name, &channel)
+        .await
+        .expect("Failed to declare queue");
+
+    bind_queue_to_exchange(&queue_name, &exchange_name, &routing_key, &channel)
+        .await
+        .expect("Failed to bind queue");
+
+    Ok(())
+}
