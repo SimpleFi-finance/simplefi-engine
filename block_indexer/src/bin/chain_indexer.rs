e05c5a151fdea283887130b36f733c5dd436783e
Filippo Vecchiato <filippovecchiato@Filippos-MBP-3.lan>
2023-04-28 16:03:55 +0100

    https method updated to use decode util

--- block_indexer/src/bin/chain_indexer.rs
+++ block_indexer/src/bin/chain_indexer.rs
@@ -1,16 +1,15 @@
-use std::{collections::HashMap, time::Instant, str::FromStr};
+use std::{collections::{HashSet}, time::Instant};
 
 use block_indexer::{utils::{get_block_logs, get_block_with_txs}, settings::load_settings};
 use chrono::{Datelike, NaiveDateTime};
-use ethabi::{Contract, RawLog, ethereum_types::H256};
 use futures::{StreamExt, TryStreamExt};
 use grpc_server::{client::AbiDiscoveryClient};
 use lapin::{options::{BasicConsumeOptions, BasicAckOptions}, types::FieldTable};
-use log::{debug, info, error};
-use rayon::{iter::ParallelIterator};
+use log::{debug, info};
+use rayon::{iter::ParallelIterator, prelude::IntoParallelIterator};
 use rayon::prelude::IntoParallelRefIterator;
 use settings::load_settings as load_global_settings;
-use shared_utils::logger::init_logging;
+use shared_utils::{logger::init_logging, decoder::{logs::evm::evm_logs_decoder, types::ContractAbi}};
 use third_parties::{
     broker::create_rmq_channel,
     mongo::{
@@ -71,114 +70,52 @@ async fn main() {
             get_block_logs(provider_url.clone(), &u64_block, &u64_block),
             get_block_with_txs(provider_url.clone(), &u64_block)
         );
-        let mut decoded_logs = vec![];
 
-        let logs_by_address = logs
-            .unwrap()
+        let logs = logs.unwrap()
             .par_iter()
-            .fold(||HashMap::new(), |mut acc, log| {
+            .map(|l| {
                 let ts = block.0.clone().unwrap().timestamp.clone();
                 let date = NaiveDateTime::from_timestamp_opt(ts, 0).unwrap();
-                let log = Log {
-                    address: log.address.clone(),
-                    block_hash: log.block_hash.clone(),
-                    block_number: log.block_number,
-                    data: log.data.clone(),
-                    log_index: log.log_index,
-                    removed: log.removed,
-                    topics: log.topics.clone(),
-                    transaction_hash: log.transaction_hash.clone(),
-                    transaction_index: log.transaction_index,
-                    transaction_log_index: log.transaction_log_index,
+                Log {
+                    address: l.address.clone(),
+                    block_hash: l.block_hash.clone(),
+                    block_number: l.block_number,
+                    data: l.data.clone(),
+                    log_index: l.log_index,
+                    removed: l.removed,
+                    topics: l.topics.clone(),
+                    transaction_hash: l.transaction_hash.clone(),
+                    transaction_index: l.transaction_index,
+                    transaction_log_index: l.transaction_log_index,
                     year: date.year() as i16,
                     month: date.month() as i8,
                     day: date.day() as i8,
                     timestamp: date.timestamp_micros(),
                     decoded_data: None,
-                    log_type: log.log_type.clone(),
-                };
-
-                acc.entry(log.address.clone().unwrap()).or_insert(vec![]).push(log);
-
-                acc
-            })
-            .reduce(||HashMap::new(), |mut acc, hm| {
-                for (key, value) in hm {
-                    acc.entry(key).or_insert(vec![]).extend(value);
+                    log_type: l.log_type.clone(),
                 }
-                acc
-            });
+            })
+            .collect::<Vec<Log>>();
         
-        let unique_addresses = Vec::from_iter(logs_by_address.keys().cloned());
+        let unique_addresses = logs.clone().into_par_iter()
+            .map(|l| l.address.unwrap())
+            .collect::<HashSet<String>>()
+            .into_iter()
+            .collect::<Vec<String>>();
 
         // todo set client dtnamically
         let mut abi_discovery_client = AbiDiscoveryClient::new("http://[::1]:50051".to_string()).await;
         let abis_addresses = abi_discovery_client.get_addresses_abi_json(unique_addresses).await;
         let abis_response = abis_addresses.into_inner();
 
-        for i in abis_response.addresses_abi {
-            let data = i;
-            let abi = &data.abi;
-            let contract: Contract = serde_json::from_str(abi.as_str()).unwrap();
-
-            let mut eventhm = HashMap::new();
-            for event in &contract.events {
-                let e = event.1[0].clone();
-                eventhm.insert(e.signature(), e);
+        let abis = abis_response.addresses_abi.into_par_iter().map(|abi| {
+            ContractAbi {
+                address: abi.address,
+                abi: abi.abi,
             }
+        }).collect::<Vec<ContractAbi>>();
 
-            let logs_of_address = logs_by_address.get(&data.address).unwrap();
-
-            logs_of_address.iter().for_each(|log| {
-                let log = log.clone();
-                let h256_topics = log.topics.iter().map(|t| H256::from_str(t).unwrap()).collect::<Vec<H256>>();
-                let bytes = hex::decode(log.data.clone().unwrap().strip_prefix("0x").unwrap()).unwrap();
-                let raw_log = RawLog {
-                    topics: h256_topics.clone(),
-                    data: bytes,
-                };
-
-                let event = eventhm.get(&h256_topics[0]);
-                match event {
-                    Some(event) => {
-                        let decoded_log = event.parse_log(raw_log);
-                        match decoded_log {
-                            #[allow(unused)]
-                            Ok(decoded_log) => {
-                                // todo push to array to save logs in mongo
-                                let decoded = Log {
-                                    address: log.address,
-                                    block_hash: log.block_hash,
-                                    block_number: log.block_number,
-                                    data: log.data,
-                                    log_index: log.log_index,
-                                    removed: log.removed,
-                                    topics: log.topics,
-                                    transaction_hash: log.transaction_hash,
-                                    transaction_index: log.transaction_index,
-                                    timestamp: log.timestamp,
-                                    year: log.year,
-                                    month: log.month,
-                                    day: log.day,
-                                    log_type: log.log_type,
-                                    transaction_log_index: log.transaction_log_index,
-                                    decoded_data: None,
-                                };
-                                decoded_logs.push(decoded);
-                            },
-                            Err(e) => {
-                                decoded_logs.push(log);
-                                error!("error: {:?}", e);
-                            }
-                        }
-                    },
-                    None => {
-                        decoded_logs.push(log.clone());
-                        error!("event not found for address: {:?}", log.address.unwrap());
-                    }
-                }
-            });
-        }
+        let decoded_logs = evm_logs_decoder(logs, abis).unwrap();
 
         debug!("Time elapsed is: {:?}ms", now.elapsed().as_millis());
 
@@ -212,6 +149,7 @@ async fn main() {
             save_logs(&db, decoded_logs),
             save_txs(&db, mongo_txs)
         );
+
         channel.basic_ack(delivery_data.delivery_tag, BasicAckOptions::default()).await.unwrap();
     }
 }
