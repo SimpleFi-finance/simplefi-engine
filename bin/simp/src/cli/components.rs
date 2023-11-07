13ccb48481612191ece1f290af3aef05827843f6
Inigo <inigo@simplefi.finance>
2023-11-07 15:04:12 +0100

    solving typo error

--- bin/simp/src/cli/components.rs
+++ bin/simp/src/cli/components.rs
@@ -20,17 +20,6 @@ pub struct SimpRpcComponents<'a> {
 /// Contains the handles to the spawned RPC servers.
 ///
 /// This can be used to access the endpoints of the servers.
-///
-/// # Example
-///
-/// ```rust
-/// use simp::cli::components::SimpRpcServerHandles;
-/// use simp::rpc::api::EthApiClient;
-/// # async fn t(handles: SimpRpcServerHandles) {
-///    let client = handles.rpc.http_client().expect("http server not started");
-///    let block_number = client.block_number().await.unwrap();
-/// # }
-/// ```
 #[derive(Debug, Clone)]
 pub struct SimpRpcServerHandles {
     /// The regular RPC server handle.
