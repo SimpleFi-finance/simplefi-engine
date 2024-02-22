9e9f053e782b36dd331cd9e344f508640ed27044
filvecchiato <fil.vecchiato@gmail.com>
2024-02-22 15:44:05 +0100

    Revert "Refactor/gold silver (#81)" (#82)
    This reverts commit da7b96bb55aa0b9ba3833fb5ef7e33bee2fe62b9.

--- crates/silver/src/dragonfly/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod daily_aggregation;
-pub mod dragonfly_driver;
-pub mod inactive_market_index;
-pub mod protocol_driver;
-pub mod protocol_status;
-pub mod volumetrics;
--- crates/silver/src/protocol_driver/driver_traits/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod market_creation_methods;
-pub mod normalize_log;
-pub mod protocol_info;
-pub mod volumetric_methods;
